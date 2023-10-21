6694e4d1265f22fd8ec9366fc07cd6da22249e8c
aduatgit <justinszigethy@aol.de>
2023-10-21 10:56:27 +0200

    refactoring part 1

---
+++ handler_users.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/aduatgit/chirpy/internal/auth"
+)
+
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password []byte `json:"-"`
+}
+
+func (cfg *apiConfig) handlerUserCreate(w http.ResponseWriter, r *http.Request) {
+	type requestBody struct {
+		Password string `json:"password"`
+		EMail    string `json:"email"`
+	}
+	type responseBody struct {
+		ID    int    `json:"id"`
+		EMail string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := requestBody{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, err.Error())
+		return
+	}
+
+	password, err := auth.HashPassword(params.Password)
+	if err != nil {
+		respondWithError(w, 400, err.Error())
+		return
+	}
+	email := params.EMail
+
+	user, err := cfg.DB.CreateUser(email, password)
+	if err != nil {
+		respondWithError(w, 400, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, responseBody{
+		ID:    user.ID,
+		EMail: user.Email,
+	})
+
+}
+
+func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Password         string `json:"password"`
+		Email            string `json:"email"`
+		ExpiresInSeconds int    `json:"expires_in_seconds"`
+	}
+	type response struct {
+		User
+		Token string `json:"token"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	user, err := cfg.DB.GetUserByEmail(params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get user")
+		return
+	}
+
+	err = auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Invalid password")
+		return
+	}
+
+	defaultExpiration := 60 * 60 * 24
+	if params.ExpiresInSeconds == 0 {
+		params.ExpiresInSeconds = defaultExpiration
+	} else if params.ExpiresInSeconds > defaultExpiration {
+		params.ExpiresInSeconds = defaultExpiration
+	}
+
+	token, err := auth.MakeJWT(user.ID, cfg.jwtSecret, time.Duration(params.ExpiresInSeconds)*time.Second)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create JWT")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, response{
+		User: User{
+			ID:    user.ID,
+			Email: user.Email,
+		},
+		Token: token,
+	})
+}
--- internal/database/database.go
+++ internal/database/database.go
@@ -17,19 +17,13 @@ type DBStructure struct {
 	Users  map[int]User  `json:"users"`
 }
 
-type Chirp struct {
-	ID   int    `json:"id"`
-	Body string `json:"body"`
-}
-
 type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID             int    `json:"id"`
+	Email          string `json:"email"`
+	HashedPassword string `json:"hashed_password"`
 }
 
 var ErrNotExist = errors.New("resource does not exist")
-var ErrAlreadyExists = errors.New("already exists")
 
 // NewDB creates a new database connection
 // and creates the database file if it doesn't exist
@@ -40,109 +34,6 @@ func NewDB(path string) (*DB, error) {
 	}
 	err := db.ensureDB()
 	return db, err
-}
-
-// CreateChirp creates a new chirp and saves it to disk
-func (db *DB) CreateChirp(body string) (Chirp, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return Chirp{}, err
-	}
-
-	id := len(dbStructure.Chirps) + 1
-	chirp := Chirp{
-		ID:   id,
-		Body: body,
-	}
-	dbStructure.Chirps[id] = chirp
-
-	err = db.writeDB(dbStructure)
-	if err != nil {
-		return Chirp{}, err
-	}
-	return chirp, nil
-}
-
-// CreateUser creates a new user and saves it to disk
-func (db *DB) CreateUser(email string, password string) (User, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return User{}, err
-	}
-
-	_, err = db.LookupUserByMail(email)
-	if err == nil {
-		return User{}, errors.New("E-Mail already registered")
-	}
-
-	id := len(dbStructure.Users) + 1
-	user := User{
-		ID:       id,
-		Email:    email,
-		Password: password,
-	}
-
-	dbStructure.Users[id] = user
-
-	err = db.writeDB(dbStructure)
-	if err != nil {
-		return User{}, err
-	}
-	return user, nil
-}
-
-// GetChirps returns all chirps in the database
-func (db *DB) GetChirps() ([]Chirp, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return nil, err
-	}
-
-	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
-	for _, chirp := range dbStructure.Chirps {
-		chirps = append(chirps, chirp)
-	}
-
-	return chirps, nil
-
-}
-
-// GetChirps returns all users in the database
-func (db *DB) GetUsers() ([]User, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return nil, err
-	}
-
-	Users := make([]User, 0, len(dbStructure.Users))
-	for _, user := range dbStructure.Users {
-		Users = append(Users, user)
-	}
-
-	return Users, nil
-}
-
-func (db *DB) UpdateUser(id int, email, hashedPassword string) (User, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return User{}, err
-	}
-
-	user, ok := dbStructure.Users[id]
-	if !ok {
-		return User{}, ErrNotExist
-	}
-
-	user.Email = email
-	user.Password = hashedPassword
-	dbStructure.Users[id] = user
-
-	err = db.writeDB(dbStructure)
-	if err != nil {
-		return User{}, err
-	}
-
-	return user, nil
 }
 
 // ensureDB creates a new database file if it doesn't exist
@@ -193,17 +84,4 @@ func (db *DB) writeDB(dbStructure DBStructure) error {
 		return err
 	}
 	return nil
-}
-
-func (db *DB) LookupUserByMail(email string) (User, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return User{}, err
-	}
-	for i := range dbStructure.Users {
-		if email == dbStructure.Users[i].Email {
-			return dbStructure.Users[i], nil
-		}
-	}
-	return User{}, errors.New("User not registered")
 }
